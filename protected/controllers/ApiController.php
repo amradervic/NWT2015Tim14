37b50bedd77596d518d4bb8e19083f377c4c8cd0
amradervic <amradervic@gmail.com>
2015-03-24 00:48:28 +0100

    REST CREATE

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -119,6 +119,67 @@ class ApiController extends Controller
     }
     public function actionCreate()
     {
+		switch($_GET['model'])
+		{
+			// Get an instance of the respective model
+			case 'kategorije':
+				$model = new Kategorija;                    
+				break;
+			case 'komentari':
+				$model = new Komentar;                    
+				break;
+			case 'korisnici':
+				$model = new Korisnik;                    
+				break;
+			case 'log':
+				$model = new Log;                    
+				break;
+			case 'objekti':
+				$model = new Objekat;                    
+				break;
+			case 'ocjene':
+				$model = new Ocjena;                    
+				break;
+			case 'privatneporuke':
+				$model = new PrivatnaPoruka;                    
+				break;
+			case 'recenzije':
+				$model = new Recenzija;                    
+				break;
+			default:
+				$this->_sendResponse(501, 
+					sprintf('Mode <b>create</b> is not implemented for model <b>%s</b>',
+					$_GET['model']) );
+					Yii::app()->end();
+		}
+		// Try to assign POST values to attributes
+		foreach($_POST as $var=>$value) {
+			// Does the model have this attribute? If not raise an error
+			if($model->hasAttribute($var))
+				$model->$var = $value;
+			else
+				$this->_sendResponse(500, 
+					sprintf('Parameter <b>%s</b> is not allowed for model <b>%s</b>', $var,
+					$_GET['model']) );
+		}
+		// Try to save the model
+		if($model->save())
+			$this->_sendResponse(200, CJSON::encode($model));
+		else {
+			// Errors occurred
+			$msg = "<h1>Error</h1>";
+			$msg .= sprintf("Couldn't create model <b>%s</b>", $_GET['model']);
+			$msg .= "<ul>";
+			foreach($model->errors as $attribute=>$attr_errors) {
+				$msg .= "<li>Attribute: $attribute</li>";
+				$msg .= "<ul>";
+				foreach($attr_errors as $attr_error)
+					$msg .= "<li>$attr_error</li>";
+				$msg .= "</ul>";
+			}
+			$msg .= "</ul>";
+			$this->_sendResponse(500, $msg );
+		}
     }
     public function actionUpdate()
     {
