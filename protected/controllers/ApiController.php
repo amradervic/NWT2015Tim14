ba1c26bea1ed7df0b382566e3013e5c4007f0c96
aimamovic3 <aimamovic3@etf.unsa.ba>
2015-03-24 01:05:44 +0100

    REST UPDATE

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -181,9 +181,72 @@ class ApiController extends Controller
 			$this->_sendResponse(500, $msg );
 		}
     }
-    public function actionUpdate()
+public function actionUpdate()
+{
+    // Parse the PUT parameters. This didn't work: parse_str(file_get_contents('php://input'), $put_vars);
+    $json = file_get_contents('php://input'); //$GLOBALS['HTTP_RAW_POST_DATA'] is not preferred: http://www.php.net/manual/en/ini.core.php#ini.always-populate-raw-post-data
+    $put_vars = CJSON::decode($json,true);  //true means use associative array
+ 
+    switch($_GET['model'])
     {
+        // Find respective model
+       
+		case 'kategorije':
+				$model = Kategorija::model()->findByPk($_GET['id']);
+				break;
+			case 'komentari':
+				$model = Komentar::model()->findByPk($_GET['id']);
+				break;
+			case 'korisnici':
+				$model = Korisnik::model()->findByPk($_GET['id']);
+				break;
+			case 'log':
+				$model = Log::model()->findByPk($_GET['id']);
+				break;
+			case 'objekti':
+				$model = Objekat::model()->findByPk($_GET['id']);
+				break;
+			case 'ocjene':
+				$model = Ocjena::model()->findByPk($_GET['id']);
+				break;
+			case 'privatneporuke':
+				$model = PrivatnaPoruka::model()->findByPk($_GET['id']);
+				break;
+			case 'recenzije':
+				$model = Recenzija::model()->findByPk($_GET['id']);
+				break;
+        default:
+            $this->_sendResponse(501, 
+                sprintf( 'Error: Mode <b>update</b> is not implemented for model <b>%s</b>',
+                $_GET['model']) );
+            Yii::app()->end();
     }
+    // Did we find the requested model? If not, raise an error
+    if($model === null)
+        $this->_sendResponse(400, 
+                sprintf("Error: Didn't find any model <b>%s</b> with ID <b>%s</b>.",
+                $_GET['model'], $_GET['id']) );
+ 
+    // Try to assign PUT parameters to attributes
+    foreach($put_vars as $var=>$value) {
+        // Does model have this attribute? If not, raise an error
+        if($model->hasAttribute($var))
+            $model->$var = $value;
+        else {
+            $this->_sendResponse(500, 
+                sprintf('Parameter <b>%s</b> is not allowed for model <b>%s</b>',
+                $var, $_GET['model']) );
+        }
+    }
+    // Try to save the model
+    if($model->save())
+        $this->_sendResponse(200, CJSON::encode($model));
+    else
+        // prepare the error $msg
+        // see actionCreate
+        // ...
+        $this->_sendResponse(500, $msg );
+}
     public function actionDelete()
     {
     }
