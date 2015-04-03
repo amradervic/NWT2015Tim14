087dd356219358ce61e9340e8be5c909c4e3b624
nkunalic2 <nkunalic2@etf.unsa.ba>
2015-04-03 22:21:32 +0200

    Angular structure

--- app/js/personController.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', ['ui.bootstrap']);
-app.controller('personCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.fullName = function() {
-        return $scope.firstName + " " + $scope.lastName;
-    }
-});
