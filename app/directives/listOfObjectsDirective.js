fd7a237742277321ac96e31d667ec87e9e005503
mkiselica1 <mkiselica1@etf.unsa.ba>
2015-05-05 00:40:14 +0200

    izmjene

--- app/directives/listOfObjectsDirective.js
+++ app/directives/listOfObjectsDirective.js
@@ -1,8 +1,12 @@
 directive.directive('ngListofobjects',[ function(){
     
     return{
-        
-        template: '<h2>{{objekt.naziv}}</h2><h5>{{objekt.adresa}}</h5><p class="list-group-item-text">{{objekt.opis}}</p>'
-    }
-}])
+        template:'<div style="horizontal-align: middle">' +
+                  '<ul class="list-group" style="width:350px">' +
+                  '<li  ng-repeat="objekt in objekti" class="span12" style="border: 4px groove #20b2aa">'+
+'<h2>{{objekt.naziv}}</h2><h5>{{objekt.adresa}}</h5><h6>Kategorija:{{objekt.tip}}</h6><p class="list-group-item-text">{{objekt.opis}}</p>'+
+                '</div>'
+    };
+}]);
 
+ 
