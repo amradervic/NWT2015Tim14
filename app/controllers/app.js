f337de49ed0490523fb66fd9cf5cdd87ca700b72
mkiselica1 <mkiselica1@etf.unsa.ba>
2015-06-03 00:44:23 +0200

    Login - radi

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -139,3 +139,18 @@ app.config(function($routeProvider, $httpProvider, $translateProvider) {
             }
         });
     }]);*/
+//app.run(['$rootScope', '$location', '$cookieStore', '$http', 'Base64',
+//    function ($rootScope, $location, $cookieStore, $http, Base64) {
+//        // keep user logged in after page refresh
+//        $rootScope.globals = $cookieStore.get('globals') || {};
+//        if ($rootScope.globals.currentUser) {
+//            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+//        }
+//  
+//        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+//            // redirect to login page if not logged in
+//            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+//                $location.path('/login');
+//            }
+//        });
+//    }]);
--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,17 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//
-loginApp.controller('LoginController',
+controllers.controller('LoginController',
     ['$scope', '$rootScope', '$location', 'LoginService',
     function ($scope, $rootScope, $location, LoginService) {
         // reset login status
-        LoginService.ClearCredentials();
-  
+       // LoginService.ClearCredentials();
+
         $scope.login = function () {
-            $scope.dataLoading = true;
+           // $scope.dataLoading = true;
             LoginService.Login($scope.username, $scope.password, function(response) {
                 if(response.success) {
                     LoginService.SetCredentials($scope.username, $scope.password);
